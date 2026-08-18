ae14532c9e3a8cc30ad6d8de2e09a077c16dac09
evera <evera@roshka.com>
2026-08-18 17:49:03 -0300

    feat(web): add socket io connection

---
+++ apps/web/src/features/home.tsx
@@ -0,0 +1,12 @@
+export function HomePage() {
+    return (
+        <>
+        <h1>Cambio</h1>
+        <p>El juego original sin powerups ni guionazos</p>
+        <div>
+            <button>Crear sala</button>
+            <button>Unirse a sala</button>
+        </div>
+        </>
+    )
+}
